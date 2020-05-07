ba18b9f5131dd2f41dd4c27df5c11d443c3cd642
acastemoreno <acastemoreno@gmail.com>
2020-05-07 15:59:11 -0500

    search avance

---
+++ helpers/search_helper.rb
@@ -0,0 +1,9 @@
+module SearchHelper
+  def render_books_in_search_page(books)
+    if books.nil?
+      erb("_empty_search__result".to_sym)
+    else
+      erb("_search_result".to_sym, locals: {books: books})
+    end
+  end
+end
