ce25353274e9085bf9ae245680fc9361d49ac992
Albert <acastemoreno@gmail.com>
2020-05-08 16:29:31 -0500

    specific fielf for search

--- helpers/api_helper.rb
+++ helpers/api_helper.rb
@@ -7,20 +7,19 @@ module ApiHelper
   BASE_URL = "https://www.googleapis.com/books/v1"
   BOOK_PER_PAGES = 40 #Max 40
 
-  def get_books(text, count:)
+  def get_books(text, count:, specific:)
     result = (count/BOOK_PER_PAGES.to_f)
       .ceil
       .times
       .inject([[], count]) do |(resp, remaining), index|
         num_books = (remaining <= BOOK_PER_PAGES ? remaining : BOOK_PER_PAGES)
-        
+        q = (specific == nil || specific == "all") ? text : "#{specific}:#{text}"
         response = JSON.parse(
           HTTP.headers(:accept => "application/json")
-            .get("#{BASE_URL}/volumes", :params => {:q => text, :startIndex => index, :maxResults => num_books})
+            .get("#{BASE_URL}/volumes", :params => {:q => q, :startIndex => index, :maxResults => num_books})
             .body
         )
-
-        [resp + response["items"], remaining - num_books]
+        [resp + Array(response["items"]), remaining - num_books]
       end
 
     result[0]
